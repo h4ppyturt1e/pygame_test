e2fdbe881bb593a7d1ddfc9940fd55b7e6c709c0
h4ppyturt1e <ryan170402@gmail.com>
2024-06-23 17:43:36 -0700

    added moving cloud and bg

--- game.py
+++ game.py
@@ -1,21 +1,47 @@
 import sys
 import pygame
 
-pygame.init()
 
-pygame.display.set_caption('test gaem')
-screen = pygame.display.set_mode((640, 480))
+class Game:
+    def __init__(self):
+        pygame.init()
 
-clock = pygame.time.Clock()
+        pygame.display.set_caption('test gaem')
+        self.screen = pygame.display.set_mode((640, 480))
 
-while True:
-    for event in pygame.event.get():
-        # gets all the events that are happening
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    pygame.display.update()
-    clock.tick(60) # forces to 60fps
-    
-    
+        self.clock = pygame.time.Clock()
+        
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
+
+        self.img_pos = [100, 200]
+        self.movement = [False, False]
+        
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
+            
+            for event in pygame.event.get():
+                # gets all the events that are happening
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+            
+            pygame.display.update()
+            self.clock.tick(60) # forces to 60fps
+        
+if __name__ == '__main__':
+    game = Game()
+    game.run()
