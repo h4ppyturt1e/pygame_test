0add34760220e7b206b173e6be6dfa1536b149e3
h4ppyturt1e <ryan170402@gmail.com>
2024-06-23 23:18:54 -0700

    added tilemap and entity collisions

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import sys
 import pygame
 
-from scripts.utils import load_image
+from scripts.utils import *
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
+
 
 class Game:
     def __init__(self):
@@ -10,25 +12,34 @@ class Game:
 
         pygame.display.set_caption('test gaem')
         self.screen = pygame.display.set_mode((640, 480))
-        
+
         self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
         self.movement = [False, False]
-        
+
         self.assets = {
+            'decor': load_images('tiles/decor'),
+            'large_decor': load_images('tiles/large_decor'),
+            'grass': load_images('tiles/grass'),
+            'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png')
         }
-        
+
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
-        
+
+        self.tilemap = Tilemap(self, tile_size=16)
+
     def run(self):
         while True:
             self.display.fill((14, 219, 210))
-            
-            self.player.update((self.movement[1] - self.movement[0], 0))
+
+            self.tilemap.render(self.display)
+
+            self.player.update(
+                self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
-            
+
             for event in pygame.event.get():
                 # gets all the events that are happening
                 if event.type == pygame.QUIT:
@@ -39,16 +50,21 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-            
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+                    
+
+            self.screen.blit(pygame.transform.scale(
+                self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
-            self.clock.tick(60) # forces to 60fps
-        
+            self.clock.tick(60)  # forces to 60fps
+
+
 if __name__ == '__main__':
     game = Game()
     game.run()
